5c44a84eb837884ea80fda3e010d81aa0385c2ee
Montse Badia <mbadia_11@hotmail.com>
2018-08-03 00:01:15 +0200

    created post route to check whether a kata solution is correct

--- routes/kata.js
+++ routes/kata.js
@@ -16,10 +16,50 @@ router.get('/random', (req, res, next) => {
       if (err) {
         return res.json(err).status(500);
       }
-      console.log(kata);
       return res.json(kata);
     });
   });
+});
+
+router.post('/:id/check', (req, res, next) => {
+  const kataId = req.params.id;
+  const inputCode = req.body.inputCode;
+
+  Kata.findById(kataId)
+    .then((kata) => {
+      const functionName = kata.functionName;
+      const tests = kata.tests;
+      const params = [];
+      const result = [];
+      const functionCall = [];
+      const evaluation = [];
+      let isCorrect = false;
+
+      tests.forEach(test => {
+        params.push(test.params);
+        result.push(test.result);
+      });
+
+      for (let x = 0; x < result.length; x++) {
+        if (typeof (params[0][0]) === 'string') { // why is params[0] an array of objects?
+          functionCall.push(functionName + '(' + '"' + params[x] + '"' + ')');
+          evaluation.push(eval(inputCode + functionCall[x]));
+        } else {
+          functionCall.push(functionName + '(' + params[x] + ')');
+          evaluation.push(eval(inputCode + functionCall[x])); // Verbessern!
+        }
+      }
+
+      evaluation.forEach((item, index) => {
+        if (item !== result[index]) {
+          isCorrect = false;
+        } else {
+          isCorrect = true;
+        };
+      });
+      console.log(isCorrect);
+    })
+    .catch(next);
 });
 
 module.exports = router;
