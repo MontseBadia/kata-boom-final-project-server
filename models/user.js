869eb61f3aebdf7ac531f786d0bc0afed79b56c3
Montse Badia <mbadia_11@hotmail.com>
2018-08-02 12:06:48 +0200

    added user model

--- models/user.js
+++ models/user.js
@@ -2,18 +2,27 @@
 
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const ObjectId = Schema.Types.ObjectId;
 
 const userSchema = new Schema({
   username: {
     type: String,
-    required: true
+    required: true,
+    unique: true
   },
   password: {
     type: String,
     required: true
-  }
-}, {
-  timestamps: true
+  },
+  katas: [{
+    kata: {
+      type: ObjectId,
+      ref: 'Kata'
+    },
+    solution: {
+      type: String
+    }
+  }]
 });
 
 const User = mongoose.model('User', userSchema);
