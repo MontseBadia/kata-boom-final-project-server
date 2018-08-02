480baaa631602152164d9edbdb4a4164b1214220
Montse Badia <mbadia_11@hotmail.com>
2018-08-02 13:00:15 +0200

    created kata model and updated user model

---
+++ models/kata.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const Mixed = Schema.Types.Mixed;
+
+const kataSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  functionName: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  tests: [{
+    params: [
+      Mixed
+    ],
+    result: {
+      type: String,
+      required: true
+    }
+  }]
+});
+
+const Kata = mongoose.model('Kata', kataSchema);
+
+module.exports = Kata;
