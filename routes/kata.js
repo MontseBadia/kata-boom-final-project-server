c7561dca4124708dcb9f0300fe2a8e446cf3be6d
Montse Badia <mbadia_11@hotmail.com>
2018-08-05 13:39:56 +0200

    added authorization checks in all /kata routes

--- routes/kata.js
+++ routes/kata.js
@@ -9,6 +9,10 @@ const Kata = require('../models/kata');
 
 // --- GET RANDOM KATA ------
 router.get('/random', (req, res, next) => {
+  if (!req.session.currentUser) {
+    return res.status(401).json({ code: 'unauthorized' });
+  }
+
   Kata.count()
     .then((count) => {
       const random = Math.floor(Math.random() * count);
@@ -16,7 +20,7 @@ router.get('/random', (req, res, next) => {
     })
     .then((kata) => {
       if (!kata) {
-        return res.status(404).json(new Error('404'));
+        return res.status(404).json(new Error('404')); // Do I need new Error here?
       }
       return res.json(kata.name);
     })
@@ -26,6 +30,11 @@ router.get('/random', (req, res, next) => {
 // --- GET ONE KATA ------
 router.get('/:name', (req, res, next) => { // Can this route be the same as the one in the front?
   const kataName = req.params.name;
+
+  if (!req.session.currentUser) {
+    return res.status(401).json({ code: 'unauthorized' });
+  }
+
   Kata.findOne({ name: kataName })
     .then((kata) => {
       if (!kata) {
@@ -43,8 +52,12 @@ router.post('/:id/check', isKataIdValid, (req, res, next) => { // Do I need to c
   const kataId = req.params.id;
   const inputCode = req.body.inputCode;
 
+  if (!req.session.currentUser) {
+    return res.status(401).json({ code: 'unauthorized' });
+  }
+
   if (!inputCode) {
-    return res.status(422).json({ code: 'no-input-provided' }); // 422 is fine?
+    return res.status(422).json({ code: 'no-input-provided' });
   }
 
   Kata.findById(kataId)
@@ -53,7 +66,7 @@ router.post('/:id/check', isKataIdValid, (req, res, next) => { // Do I need to c
       res.status(200).json(data);
     })
     .catch(() => {
-      return res.status(422).json({ code: 'unexpected-identifier' }); // Is 422 ok?
+      return res.status(422).json({ code: 'unexpected-identifier' }); // Is 422 ok? How to I include the 500 here?
     });
 });
 
